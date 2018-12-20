5ffe11ca5a071389a1d9414e8246adbfa74b56e7
Michael Leslie Johnson <MichaelLeslie.Johnson@uscga.edu>
2018-12-19 22:12:08 -0600

    Added variable min_dist

--- Dijkstras/Dijkstras/Dijkstras.py
+++ Dijkstras/Dijkstras/Dijkstras.py
@@ -1,5 +1,4 @@
-import csv
-
+# Graph Class written by econchick
 class Graph:
   def __init__(self):
     self.nodes = list()
@@ -15,11 +14,17 @@ class Graph:
     self.distance[(from_node, to_node)] = distance
     self.distance[(to_node, from_node)] = distance
 
-def dijkstra2(graph, initial):
+
+def dijkstra(graph, initial):
     distToNode = [(initial, 0, initial)]
 
     notVisited = list(graph.nodes)
     notVisited.remove(initial)
+    min_value = 0
+    min_node = None
+
+    for arc in graph.distance:
+        min_value += graph.distance[arc]
 
     while notVisited:
         table = dict()
@@ -36,8 +41,6 @@ def dijkstra2(graph, initial):
                     table[node] = None
 
         # Find min distance to src
-        min_value = 99999999
-        min_node = None
         for node in table:
             if (table[node] != None):
                 if (table[node][0] < min_value):
@@ -55,57 +58,3 @@ def dijkstra2(graph, initial):
     # Return the list of tuples 
     return distToNode
 
-myGraph = Graph()
-
-# Add Nodes
-myGraph.add_node("t")
-myGraph.add_node("u")
-myGraph.add_node("v")
-myGraph.add_node("w")
-myGraph.add_node("x")
-myGraph.add_node("y")
-myGraph.add_node("z")
-
-# Add Edges
-myGraph.add_edge("z", "x", 8)
-myGraph.add_edge("z", "y", 12)
-myGraph.add_edge("x", "y", 6)
-myGraph.add_edge("y", "t", 7)
-myGraph.add_edge("y", "v", 8)
-myGraph.add_edge("v", "t", 4)
-myGraph.add_edge("x", "w", 6)
-myGraph.add_edge("w", "v", 4)
-myGraph.add_edge("w", "u", 3)
-myGraph.add_edge("v", "u", 3)
-myGraph.add_edge("u", "t", 2)
-
-outFile = open("dijkstra solutions.csv", "w")
-output = csv.writer(outFile)
-
-# Print Column headings 
-print("\t", end="")
-colHeading = [""]
-for node in myGraph.nodes:
-    print("%s\t" % node, end="")
-    colHeading.append(node)
-
-output.writerow(colHeading)
-print()
-
-pathDict = dict()
-
-# print lines
-for srcNode in myGraph.nodes:
-    print("%s\t" % srcNode, end="")
-    myLine = [srcNode]
-    solution = dijkstra2(myGraph, srcNode)
-    for node in solution:
-        pathDict[node[2][-1]] = node[1]
-    for destNode in myGraph.nodes:
-        print("%d\t" % pathDict[destNode], end="")
-        myLine.append(pathDict[destNode])
-
-    output.writerow(myLine)
-    print()
-
-outFile.close()
